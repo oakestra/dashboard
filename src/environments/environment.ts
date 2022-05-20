fc22d6df4908606915d6068b22ee4387138e2507
maria <marianievas11@gmail.com>
2022-05-20 13:45:17 +0200

    develop branch

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: "http://127.0.0.1:10000/frontend"
+//  apiUrl: "http://127.0.0.1:10000/frontend"
+  apiUrl: "http://localhost:10000/frontend"
 };
