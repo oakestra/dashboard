23ccd6cf014ade6866e3304fd562e95df31e18a4
maria <marianievas11@gmail.com>
2022-06-13 18:19:14 +0200

    changes from main

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
 //  apiUrl: "http://127.0.0.1:10000/frontend"
-  apiUrl: "http://localhost:10000/frontend"
+  apiUrl: "http://localhost:10000/api"
 };
+
