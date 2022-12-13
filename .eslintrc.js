885987f80c723820d73e237c21e53e32ce5c2af3
Daniel <danimair9@gmail.com>
2022-12-13 17:50:51 +0100

    try to fix GitHub linting bug

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
         node: true,
         mocha: true,
     },
-    ignorePatterns: ['dist/', 'types/', 'node_modules/', '.eslintrc.js', '*.config.[jt]s'],
+    ignorePatterns: ['dist/', 'types/', 'node_modules/', '.eslintrc.js', '*.config.[jt]s', 'assets/', 'docker/'],
     extends: [
         'eslint:recommended',
         'plugin:@typescript-eslint/recommended',
