15962eac6548a429937f7796476976178b565c7d
Daniel <danimair9@gmail.com>
2023-03-05 15:52:00 +0100

    fixed linting

--- src/app/control/infrastructure/infrastructure.component.ts
+++ src/app/control/infrastructure/infrastructure.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-infrastructure',
-  templateUrl: './infrastructure.component.html',
-  styleUrls: ['./infrastructure.component.css']
+    selector: 'app-infrastructure',
+    templateUrl: './infrastructure.component.html',
+    styleUrls: ['./infrastructure.component.css'],
 })
-export class InfrastructureComponent {
-
-}
+export class InfrastructureComponent {}
