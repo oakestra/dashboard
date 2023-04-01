3ebdce8d2337746c2828e1d10f396b794dae828d
Daniel <danimair9@gmail.com>
2023-04-01 13:39:04 +0200

    added download config button

--- src/app/control/dialogs/edit-user/dialogEditUser.ts
+++ src/app/control/dialogs/edit-user/dialogEditUser.ts
@@ -87,6 +87,31 @@ export class DialogEditUserView {
     closeDialog() {
         this.dialogRef.close({ event: DialogAction.CANCEL });
     }
+
+    downloadCredentials() {
+        const setting = {
+            element: {
+                dynamicDownload: null as any,
+            },
+        };
+
+        const config = {
+            ...this.form.getRawValue(),
+        };
+        delete config.roles;
+        console.log(config);
+
+        const fileName = 'credentials.json';
+        if (!setting.element.dynamicDownload) {
+            setting.element.dynamicDownload = document.createElement('a');
+        }
+        const element = setting.element.dynamicDownload;
+        const fileType = fileName.indexOf('.json') > -1 ? 'text/json' : 'text/plain';
+        element.setAttribute('href', `data:${fileType};charset=utf-8,${encodeURIComponent(JSON.stringify(config))}`);
+        element.setAttribute('download', fileName);
+        const event = new MouseEvent('click');
+        element.dispatchEvent(event);
+    }
 }
 
 export class UserValidators {
