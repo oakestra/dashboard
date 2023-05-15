da666d7c4f9cdc54bfce4645f021fe78c55a5a64
Daniel <danimair9@gmail.com>
2023-05-15 18:50:28 +0200

    updated env var name

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
     // eslint-disable-next-line
-    apiUrl: `http://${(window as any)['env']['apiUrl']}/api` || 'https://localhost:10000',
+    apiUrl: `http://${(window as any)['env']['apiIP']}/api` || 'https://localhost:10000',
 };
