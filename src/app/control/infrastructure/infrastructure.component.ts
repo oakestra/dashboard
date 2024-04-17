8d3c6355005b19f0d423a552e48ac0b49afaa683
Giovanni Bartolomeo <giobarty@gmail.com>
2024-04-17 12:32:41 +0200

    added grafana link

--- src/app/control/infrastructure/infrastructure.component.ts
+++ src/app/control/infrastructure/infrastructure.component.ts
@@ -1,8 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
     selector: 'app-infrastructure',
     templateUrl: './infrastructure.component.html',
     styleUrls: ['./infrastructure.component.scss'],
 })
-export class InfrastructureComponent {}
+export class InfrastructureComponent {
+
+    public grafanaApi: string = "";
+
+    ngOnInit() {
+        let parts: string[] = environment.apiUrl.split(":");
+        this.grafanaApi = "http:" + parts[1] + ":3000" ;
+        window.open(this.grafanaApi, "_blank");
+        window.location.href = "/control";
+    }
+
+}
