b91d2a1e9239484cba4e24881e561608daa58c23
Matthew Humphreys <ge65yay@tum.de>
2026-05-24 14:37:58 +0200

    enable https forwarding
    Signed-off-by: Matthew Humphreys <ge65yay@tum.de>

--- dist/oakestra-dashboard/assets/env.config.js
+++ dist/oakestra-dashboard/assets/env.config.js
@@ -3,5 +3,6 @@
 
   // Environment variables
   window["env"]["apiIP"] = "${API_ADDRESS}";
-  window["env"]["grafanaUrl"] = "http://${GRAFANA_ADDRESS}";
+  window["env"]["grafanaAddress"] = "${GRAFANA_ADDRESS}";
+  window["env"]["httpsEnabled"] = "${HTTPS_ENABLED}";
 })(this);
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,11 @@
 export const environment = {
     production: true,
-    // eslint-disable-next-line
-    apiUrl: `http://${(window as any)['env']['apiIP']}/api` || 'https://localhost:10000',
-    grafanaUrl: (window as any)['env']['grafanaUrl'] as string,
+    apiUrl: (() => {
+        const protocol = (window as any)['env']['httpsEnabled'] === 'true' ? 'https' : 'http';
+        return `${protocol}://${(window as any)['env']['apiIP']}/api`;
+    })(),
+    grafanaUrl: (() => {
+        const protocol = (window as any)['env']['httpsEnabled'] === 'true' ? 'https' : 'http';
+        return `${protocol}://${(window as any)['env']['grafanaAddress']}`;
+    })(),
 };
